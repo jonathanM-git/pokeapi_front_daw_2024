786321bfb30601efa25429f56b247088c1ba9bd2
jonathanM-git <minguezjonathan.27@gmail.com>
2025-12-18 17:43:17 +0100

    prueba examen_corregido

--- src/views/PokemonDetail.vue
+++ src/views/PokemonDetail.vue
@@ -1,5 +1,59 @@
 <template>
- 
+  <main class="pokemon-detail" v-if="pokemon">
+    <button class="back-button" @click="goBack">Volver</button>
+    <div class="detail-header">
+      <h1>{{ capitalizedName }}</h1>
+      <span class="identifier">#{{ pokemon.id }}</span>
+    </div>
+
+    <section class="sprites">
+      <img :src="pokemon.sprites.front_default" alt="Sprite frontal" />
+      <img :src="pokemon.sprites.back_default" alt="Sprite trasero" v-if="pokemon.sprites.back_default" />
+      <img
+        :src="pokemon.sprites.other['official-artwork'].front_default"
+        alt="Arte oficial"
+        v-if="pokemon.sprites.other?.['official-artwork']?.front_default"
+      />
+    </section>
+
+    <section class="description" v-if="description">
+      <h2>Descripción</h2>
+      <p>{{ description }}</p>
+    </section>
+
+    <section class="detail-grid">
+      <div class="detail-card">
+        <h3>Tipos</h3>
+        <ul>
+          <li v-for="type in pokemon.types" :key="type.slot">{{ type.type.name }}</li>
+        </ul>
+      </div>
+
+      <div class="detail-card">
+        <h3>Habilidades</h3>
+        <ul>
+          <li v-for="ability in pokemon.abilities" :key="ability.ability.name">{{ ability.ability.name }}</li>
+        </ul>
+      </div>
+
+      <div class="detail-card">
+        <h3>Estadísticas base</h3>
+        <ul>
+          <li v-for="stat in pokemon.stats" :key="stat.stat.name">
+            {{ stat.stat.name }}: {{ stat.base_stat }}
+          </li>
+        </ul>
+      </div>
+
+      <div class="detail-card">
+        <h3>Movimientos destacados</h3>
+        <ul>
+          <li v-for="move in displayedMoves" :key="move.move.name">{{ move.move.name }}</li>
+        </ul>
+      </div>
+    </section>
+  </main>
+  <div v-else class="loading-state">Cargando información del pokémon...</div>
 </template>
 
 <script setup>
